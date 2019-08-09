ecfc871ffc1b72ce4f360521e14407b7ab34dc6e
Yolanda Lopez <yolita2402@gmail.com>
2019-08-08 23:44:33 -0600

    Agregar demo variables

---
+++ demo-terraform2/aws-autoscaling/variables.tf
@@ -0,0 +1,18 @@
+variable "name_prefix" {
+}
+variable "ami_id" {
+}
+variable "instance_type" {
+  
+}
+
+variable "autoscaling_name" {
+}
+variable "min_size" {
+}
+variable "max_size" {
+}
+variable "availability_zones" {
+  
+}
+
